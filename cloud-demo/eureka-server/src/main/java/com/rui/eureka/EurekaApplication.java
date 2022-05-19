406f737a2e82159ee5881243fa1746d490c4aca4
rui <azernia@foxmail.com>
2022-05-19 18:01:18 +0800

    搭建EurekaServer

---
+++ cloud-demo/eureka-server/src/main/java/com/rui/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.rui.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * create date 2022/5/19 17:50
+ *
+ * @author rui
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
