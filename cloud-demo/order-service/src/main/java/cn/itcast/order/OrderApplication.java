f69b8bfd6bac071afbba159571970b779eb69543
rui <azernia@foxmail.com>
2022-05-19 17:28:06 +0800

    跨服务远程调用

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -3,13 +3,24 @@ package cn.itcast.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+/**
+ * 启动类也是一个配置类
+ */
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    // 使 SpringBoot 支持发送 HTTP 请求
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,18 +2,32 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
 
+    private final OrderMapper orderMapper;
+
+    private final RestTemplate restTemplate;
+
     @Autowired
-    private OrderMapper orderMapper;
+    public OrderService(OrderMapper orderMapper, RestTemplate restTemplate) {
+        this.orderMapper = orderMapper;
+        this.restTemplate = restTemplate;
+    }
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+        // 2.利用 RestTemplate 发送 http 请求
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        // 3.发送 http 请求
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
         // 4.返回
         return order;
     }
