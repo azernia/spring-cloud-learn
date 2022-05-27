60dcd5baaa9d0c8fb35d89cc6c45e1d88754ed5e
rui <azernia@foxmail.com>
2022-05-27 15:43:57 +0800

    创建Spring cloud 项目

--- create-cloud/NOTE.md
+++ create-cloud/NOTE.md
@@ -1,3 +1,30 @@
 # 创建 SpringCloud 项目
 ## 创建分布式架构
 - maven 聚合项目
+
+## 使用框架版本
+- 基本框架版本
+| Spring Cloud Alibaba Version | Spring Cloud Version     | Spring Boot Version |
+|------------------------------|--------------------------|---------------------|
+| 2.2.7.RELEASE                | Spring Cloud Hoxton.SR12 | 2.3.12.RELEASE      |
+
+- 框架依赖组件版本
+
+| Spring Cloud Alibaba Version | Sentinel Version | Nacos Version | RocketMQ Version | Dubbo Version | Seata Version |
+|------------------------------|------------------|---------------|------------------|---------------|---------------|
+| 2.2.7.RELEASE                | 1.8.1            | 2.0.3         | 4.6.1            | 2.7.13        | 1.3.0         |
+
+## 父工程添加依赖
+```xml
+<dependencyManagement>
+    <dependencies>
+        <dependency>
+            <groupId>com.alibaba.cloud</groupId>
+            <artifactId>spring-cloud-alibaba-dependencies</artifactId>
+            <version>2.2.7.RELEASE</version>
+            <type>pom</type>
+            <scope>import</scope>
+        </dependency>
+    </dependencies>
+</dependencyManagement>
+```
--- create-cloud/order-center/src/main/java/com/rui/order/controller/OrderController.java
+++ create-cloud/order-center/src/main/java/com/rui/order/controller/OrderController.java
@@ -1,9 +1,7 @@
 package com.rui.order.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * create date 2022/5/27 14:04
@@ -14,17 +12,18 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/order")
 public class OrderController {
 
-    private final RestTemplate restTemplate;
+    // private final RestTemplate restTemplate;
 
-    @Autowired
-    public OrderController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    // @Autowired
+    // public OrderController(RestTemplate restTemplate) {
+    //     this.restTemplate = restTemplate;
+    // }
 
     @RequestMapping("/add")
     public String add() {
-        String forObject = restTemplate.getForObject("http://localhost:8081/stock/add", String.class);
-        return "下单成功 " + forObject;
+        // String forObject = restTemplate.getForObject("http://localhost:8081/stock/add", String.class);
+        // return "下单成功 " + forObject;
+        return "下单成功";
     }
 
 }
